2954180777fc80406075d65fee6ee590fcf9c31b
Bretton Fry <Bretton.fry@gmail.com>
2021-04-20 16:36:49 -0400

    Update contact_submit.php

--- php/contact_submit.php
+++ php/contact_submit.php
@@ -3,7 +3,7 @@
 // Brett wrote this mailer from scratch
 
     header('refresh: 5; url=../contact.html');
-    echo "<link href='../css/master.css' rel='stylesheet'><script src='https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.5.3/lottie.js'></script><script src='../js/lottie_call.js'></script>"; // Calls CSS and Lottie through echo.
+    echo "<link href='../css/master.css' rel='stylesheet'><script src='../js/lottie.js'></script><script src='../js/lottie_call.js'></script>"; // Calls CSS and Lottie through echo.
     
     $sender = $_POST['yourname'];
     $reply = $_POST['youremail'];
