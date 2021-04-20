6838a1141c9af46a237502d7ec783c9c14aa4474
brettonambrose <Bretton.fry@gmail.com>
2021-04-20 16:56:44 -0400

    pulled_lottie

--- js/lottie_call.js
+++
@@ -1,7 +0,0 @@
-const animation = bodymovin.loadAnimation({
-    container: document.getElementById('animate'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: '../video/data.json'
-});
